c48c3757605bb1d2faa3134fcbd8a31901b53691
Gorka Ortega <motxobe1966@gmail.com>
2024-09-21 17:20:40 +0200

    He añadido un archivo qu se puede borrar

--- WebApiTiendaVideojuegos/obj/Debug/net8.0/WebApiTiendaVideojuegos.AssemblyInfo.cs
+++ WebApiTiendaVideojuegos/obj/Debug/net8.0/WebApiTiendaVideojuegos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiTiendaVideojuegos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4baa4be52e792628e4b61eb6a0ed8cf66a069f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00b89720aaec0bf88ca43894ed1ab84fc2fe95e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiTiendaVideojuegos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiTiendaVideojuegos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
