96711c7a01a01deb8db5a48fa1c0281ce563a1ce
ibaivalente <ibaivalentelavado@gmail.com>
2024-09-23 09:29:32 +0200

    GetJuegos añadido

---
+++ WebApiTiendaVideojuegos/Controllers/JuegosController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiTiendaVideojuegos.DTOs;
+using WebApiTiendaVideojuegos.Models;
+
+namespace WebApiTiendaVideojuegos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JuegosController : ControllerBase
+    {
+        private readonly MiTiendaVideojuegosContext context;
+
+        public JuegosController (MiTiendaVideojuegosContext context)
+        {
+            this.context = context;
+        }
+
+        // CRUD inicial
+        // Obtener todos los juegos
+        [HttpGet]
+        public async Task<ActionResult> GetJuegos ()
+        {
+            var juegos = await context.Juegos.Select(x => new DTOJuegos
+            {
+                IdJuego = x.IdJuego,
+                Nombre = x.Nombre,
+                Precio = x.Precio,
+                Disponible = x.Disponible,
+                Lanzamiento = x.Lanzamiento,
+                Pegi = x.Pegi,
+                IdCategoria = x.IdCategoria,
+                IdDesarrolladora = x.IdDesarrolladora,
+                IdPlataforma = x.IdPlataforma
+            }).ToListAsync();
+
+            return Ok(juegos);
+        }
+
+        // Crear un juego
+        [HttpPost]
+        public async Task<ActionResult> PostJuego ()
+        {
+            var nuevoJuego = await context.Juegos.Select( x => new )
+        }
+
+    }
+}
