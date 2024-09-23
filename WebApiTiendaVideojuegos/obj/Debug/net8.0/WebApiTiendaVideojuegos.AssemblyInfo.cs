e38ba153f27768efb57e1748eab14a11743b0bfc
Alex Basurto <alexbasurto1@gmail.com>
2024-09-23 09:39:39 +0200

    subir cambios

--- WebApiTiendaVideojuegos/obj/Debug/net8.0/WebApiTiendaVideojuegos.AssemblyInfo.cs
+++ WebApiTiendaVideojuegos/obj/Debug/net8.0/WebApiTiendaVideojuegos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiTiendaVideojuegos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc2ef9a593242ccfc77baa6143b3332409a6ebee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a8ee8d0f692f428fdfdb6f500a6a0b0da314cf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiTiendaVideojuegos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiTiendaVideojuegos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
