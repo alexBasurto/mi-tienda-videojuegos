1cf37b2f61393e9b16b233bb209df8d80cda2fb3
Alex Basurto <alexbasurto1@gmail.com>
2024-09-23 09:36:47 +0200

    ramaAlex y dev merge

--- WebApiTiendaVideojuegos/obj/Debug/net8.0/WebApiTiendaVideojuegos.AssemblyInfo.cs
+++ WebApiTiendaVideojuegos/obj/Debug/net8.0/WebApiTiendaVideojuegos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiTiendaVideojuegos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa1d08815bf8331795c4f2a223745ddee826fa4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+183300084729c88e51b027d2d9ff389439256c8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiTiendaVideojuegos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiTiendaVideojuegos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
